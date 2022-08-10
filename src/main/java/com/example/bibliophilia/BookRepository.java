ebc03ce7669b1e28cd2f21e4cdcf49464836aecf
noobDevHL <pcaline@web.de>
2022-08-10 14:38:01 +0200

    Navbar- und Footer-Fragments angelegt und in allBooks.html integriert. Bootstrap eingebunden.

--- src/main/java/com/example/bibliophilia/BookRepository.java
+++ src/main/java/com/example/bibliophilia/BookRepository.java
@@ -1,11 +1,12 @@
 package com.example.bibliophilia;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import org.yaml.snakeyaml.events.Event;
 
 // Repository: Abstraktion von Spring, um einfach auf DB zuzugreifen
 @Repository
-public interface BookRepository extends CrudRepository<Book, Long> {
+public interface BookRepository extends JpaRepository<Book, Long> {
 
 }
